4b2c7a7884df9f2fcbb224a97ff700e2f27326dd
vig27131 <62382017+vig27131@users.noreply.github.com>
2026-04-23 09:05:09 +0200

    Replace wrong with correct version cpp_task_fixed.cpp
    Somehow the wrong file was uploaded. Replacing with correct version.

--- Task1/cpp_task_fixed.cpp
+++ Task1/cpp_task_fixed.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <atomic>
+#include <memory>
 #include <thread>
 #include <functional>
 #include <iostream>
@@ -15,61 +16,62 @@ void StartThread(
     thread = std::thread(
         [&, Process, timeout]()
         {
-            auto start = std::chrono::steady_clock::now();
-
-            while (running.load())
+            auto start = std::chrono::high_resolution_clock::now();
+            while(running)
             {
                 bool aborted = Process();
 
-                auto now = std::chrono::steady_clock::now();
-                auto duration = now - start;
-
+                auto end = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
                 if (aborted || duration > timeout)
                 {
-                    running.store(false);
+                    running = false;
                     break;
                 }
             }
         });
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    std::atomic<bool> running1 = true;
-    std::atomic<bool> running2 = true;
-
+    std::atomic<bool> my_running = true;
     std::thread my_thread1, my_thread2;
     int loop_counter1 = 0, loop_counter2 = 0;
 
+    // start actions in seprate threads and wait of them
+
     StartThread(
         my_thread1,
-        running1,
+        my_running, 
         [&]()
         {
-            std::this_thread::sleep_for(2000ms);
+            // "some actions" simulated with waiting
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
             loop_counter1++;
             return false;
         },
-        10s);
+        10s); // loop timeout
 
     StartThread(
         my_thread2,
-        running2,
+        my_running, 
         [&]()
         {
+            // "some actions" simulated with waiting 
             if (loop_counter2 < 5)
             {
-                std::this_thread::sleep_for(2000ms);
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                 loop_counter2++;
                 return false;
             }
             return true;
         },
-        10s);
+        10s); // loop timeout
+
 
     my_thread1.join();
     my_thread2.join();
 
-    std::cout << "C1: " << loop_counter1
-              << " C2: " << loop_counter2 << std::endl;
+    // print execlution loop counters
+    std::cout << "C1: " << loop_counter1 << " C2: " << loop_counter2 << std::endl;
 }
